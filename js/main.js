7e67eecbb333cefda38c357df6de3407dd27f318
Andaksindan <gogogedam@gmail.com>
2026-01-30 17:12:26 +0530

    v3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 async function loadComponent(id, file) {
-  const response = await fetch(file);
-  const html = await response.text();
-  document.getElementById(id).innerHTML = html;
+  try {
+    const response = await fetch(`${file}?v=${new Date().getTime()}`);
+    if (!response.ok) throw new Error(`Failed to load ${file}`);
+    const html = await response.text();
+    document.getElementById(id).innerHTML = html;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 loadComponent("header", "/components/header.html");
-loadComponent("footer", "/components/footer.html");
 
 /* Lazy-load blur images */
 document.addEventListener("DOMContentLoaded", () => {
